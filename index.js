bf2531f898e671c567c42981fa8a8f95e2b7236c
laura-paullette <laura.paullette@student.moringaschool.com>
2023-03-13 20:41:46 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,2 +1,17 @@
 
 // Write your code here
+const num1 = 2 
+const num2 = 31
+
+const multiply= num1 * num2
+
+const random= 4
+
+let num3 = 95
+let num4 = 13
+let mod =(num3%= num4)
+
+let max = 20
+math.max(0,20)
+
+
